f56d50e11cc709eeb497fc2265aab168fdc9859f
dipti <dipti.yadav@outlook.com>
2021-06-07 10:26:36 -0400

    Curring method

---
+++ A Chain adding function.js
@@ -0,0 +1,11 @@
+function add(x){
+    let sum = x;
+    function resultFn(y){
+        sum += y;
+        return resultFn;
+    }
+    resultFn.valueOf = function(){
+            return sum;
+        };
+    return resultFn;
+}
