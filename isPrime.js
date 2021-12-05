e1cb664dfd7edd95e5e5b2afdca45d977f254938
dipti <dipti.yadav@outlook.com>
2021-12-05 15:49:52 -0500

    isPrime

---
+++ isPrime.js
@@ -0,0 +1,31 @@
+//console.log(Math.ceil((2 ** 0.5)))
+// we can also use Math.floor and For square root Math.sqrt
+// Math.floor  (ex => 4.5 then it gives 4)
+function isPrime(ele) {
+  for (let i = 2; i <= Math.ceil(ele ** 0.5); i++) {
+    if (ele % i === 0 && i !== ele) return false;
+  }
+  return true;
+}
+console.log(isPrime(2));
+
+// to flaten the array
+let factors = [
+  [2],
+  [3],
+  [2, 2],
+  [5],
+  [2, 3],
+  [7],
+  [2, 2, 2],
+  [3, 3],
+  [2, 5],
+  [11],
+  [2, 2, 3],
+  [13],
+  [2, 7],
+  [3, 5],
+  [2, 2, 2, 2],
+  [17],
+];
+console.log(factors.flat());
