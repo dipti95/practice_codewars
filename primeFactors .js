63334e75eefb9598ac585d9c93b47ec17cab4eab
dipti <dipti.yadav@outlook.com>
2021-12-31 23:09:15 -0500

    prime Factor

--- primeFactors .js
+++ primeFactors .js
@@ -1,61 +1,62 @@
-const primeFactors = n => {
-    if (n < 2) return `(${n})`;
-    let factors = '';
-    for ( let i = 2; i <= n; i++) {
-      let count = 0;
-      while ( n%i === 0 ) {
-        count++; 
-        n /= i;
-      }
-      if (count) {
-        factors += `(${i}`;
-        if (count > 1) factors += `**${count}`;
-        factors += `)`;
-      }
+const primeFactors = (n) => {
+  if (n < 2) return `(${n})`;
+  let factors = "";
+  for (let i = 2; i <= n; i++) {
+    let count = 0;
+    while (n % i === 0) {
+      count++;
+      n /= i;
     }
-    return factors;
+    if (count) {
+      factors += `(${i}`;
+      if (count > 1) factors += `**${count}`;
+      factors += `)`;
+    }
   }
-  // another way 
-  function primeFactors(n) {
-    var str = ""
-    var k = 2;
-    var count = 0;
-  
-    while (k <= n) {
-      while (n % k == 0) {
-        n /= k;
-        count++;
-      }
-      if (count == 0) {
-        k++;
-        continue;
-      }
-  
-      if (count == 1) { str += `(${k})` }
-      else str += `(${k}**${count})`
-      count = 0;
-      k++;
-  }
-    return str;
-  }
+  return factors;
+};
+// another way
+function primeFactors(n) {
+  let str = "";
+  let k = 2;
+  let count = 0;
 
-  // one more way but time complexity is more ;
-  // This solution is by using hashmap;
-  // In this space complexity is also more ;
-  function primeFactors(n){
-  const factors = []; 
-  let hash = {}
-  let str = ''
+  while (k <= n) {
+    while (n % k == 0) {
+      n /= k;
+      count++;
+    }
+    if (count == 0) {
+      k++;
+      continue;
+    }
+
+    if (count == 1) {
+      str += `(${k})`;
+    } else str += `(${k}**${count})`;
+    count = 0;
+    k++;
+  }
+  return str;
+}
+
+// one more way but time complexity is more ;
+// This solution is by using hashmap;
+// In this space complexity is also more ;
+function primeFactors(n) {
+  const factors = [];
+  let hash = {};
+  let str = "";
   for (let i = 2, x = Math.abs(n); i <= x; i++)
     while (n % i === 0) {
       factors.push(i);
-      if(i in hash) hash[i] += 1;
-      else hash[i] =1
+      if (i in hash) hash[i] += 1;
+      else hash[i] = 1;
       n /= i;
     }
- for (const [key, value] of Object.entries(hash)) {
-    if(value === 1) str += '('+key+')'
-    else str += '('+key+"**"+value+')';
+  for (const [key, value] of Object.entries(hash)) {
+    if (value === 1) str += "(" + key + ")";
+    else str += "(" + key + "**" + value + ")";
   }
-    return str
+  return str;
 }
