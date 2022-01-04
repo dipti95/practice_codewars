2857d62ffb4480ca3b072f62c8de4c56e2212bf9
dipti <dipti.yadav@outlook.com>
2022-01-03 21:15:07 -0500

    type

--- RandomProblems/type.js
+++ RandomProblems/type.js
@@ -1,5 +1,7 @@
 // return type of val without using typeof
 
 function type(val) {
+  // Object.prototype.toString.call(val) return [object typeofVal]
+  // that's why we need to slice the return value;
   return Object.prototype.toString.call(val).slice(8, -1);
 }
